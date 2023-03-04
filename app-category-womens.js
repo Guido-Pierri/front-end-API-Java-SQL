51ab4424427afb64515bd30f9b334bd33f662480
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-03-04 17:05:46 +0100

    right name on womens category class product

--- app-category-womens.js
+++ app-category-womens.js
@@ -94,7 +94,7 @@ function renderJson(json1) {
 
         if (category === "women's clothing") {
             productsEl.innerHTML += `
-            <div class="producct card-group border-white flex-column m-5 justify-content-center" style="align-items: flex-start;" >
+            <div class="product card-group border-white flex-column m-5 justify-content-center" style="align-items: flex-start;" >
             <div class='card border-0'>
             <img class="card-img-top img-fluid" src="${productImage}">
             <div class="card-img-overlay">
