7a4fb9396613f96e90c0c9cbf9aef5e0e291a903
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-08-01 18:10:46 +0200

    new API fetch route

--- app.js
+++ app.js
@@ -32,68 +32,65 @@
     InlämningUppgiften lämnas in inlämningslådan på kurswebbplatsen Totara:1.All din källkod i en komprimerad mapp (ZIP-fil)2.Länk till publicerad webbplats3.Länk till publicerat repositorypå Github
     */
 
-"use strict";
+'use strict'
 
-const productsEl = document.getElementById("products");
-const menEl = document.getElementById("men");
-const basketlistEl = document.getElementById("basketlist");
-const basketEl = document.getElementById("basket");
-const subtotalEl = document.getElementById("subtotal");
-const totalItemsInCartEl = document.getElementById("totalitemsincart");
-const carticonEl = document.getElementById("carticon");
-const checkoutbuttonEl = document.getElementById("checkoutbutton");
-const modalEl = document.getElementById("modal");
-const modalBasketEl = document.getElementById("offcanvasWithBothOptions");
+const productsEl = document.getElementById('products')
+const menEl = document.getElementById('men')
+const basketlistEl = document.getElementById('basketlist')
+const basketEl = document.getElementById('basket')
+const subtotalEl = document.getElementById('subtotal')
+const totalItemsInCartEl = document.getElementById('totalitemsincart')
+const carticonEl = document.getElementById('carticon')
+const checkoutbuttonEl = document.getElementById('checkoutbutton')
+const modalEl = document.getElementById('modal')
+const modalBasketEl = document.getElementById('offcanvasWithBothOptions')
 
-
-console.log(localStorage.getItem("USD"));
-getRate();
+console.log(localStorage.getItem('USD'))
+getRate()
 function getRate() {
-    fetch('https://api.valuta.se/api/sek/rates/')
-        .then(res => res.json())
-        .then(data => render(data));
+  fetch('https://api.valuta.se/api/sek/rates/')
+    .then((res) => res.json())
+    .then((data) => render(data))
 
-    function render(rates) {
-        console.log(rates);
-        let arr = rates;
-        console.log("arr", arr);
-        console.log("arr", arr[4].value);
-        localStorage.setItem("USD", (arr[4].value) / 100);
-    }
+  function render(rates) {
+    console.log(rates)
+    let arr = rates
+    console.log('arr', arr)
+    console.log('arr', arr[4].value)
+    localStorage.setItem('USD', arr[4].value / 100)
+  }
 }
 
-fetch('http://localhost:8080/products')
-    .then(res => res.json())
-    .then(json1 => renderJson(json1))
-    .catch(error => console.log(error));
+fetch('https://app-produkt-api-230801161903.azurewebsites.net/products')
+  .then((res) => res.json())
+  .then((json1) => renderJson(json1))
+  .catch((error) => console.log(error))
 
 function renderJson(json1) {
-    let products = json1;
-    console.log(products);
+  let products = json1
+  console.log(products)
 
-    //fetch currency exchane
+  //fetch currency exchane
 
+  localStorage.setItem('products', JSON.stringify(products))
 
+  productsEl.innerHTML = ''
+  for (let i = 0; i < products.length; i++) {
+    let productTitle = products[i].title
+    let productImage = products[i].image
+    let description = products[i].description
+    let category = products[i].category
+    //let rate = products[i].rating.rate;
+    //let count = products[i].rating.count;
 
-    localStorage.setItem('products', JSON.stringify(products));
+    let price = products[i].price
 
-    productsEl.innerHTML = "";
-    for (let i = 0; i < products.length; i++) {
-        let productTitle = products[i].title;
-        let productImage = products[i].image;
-        let description = products[i].description;
-        let category = products[i].category;
-        //let rate = products[i].rating.rate;
-        //let count = products[i].rating.count;
+    let productId = products[i].id
+    // console.log("json:",jsonobject);
+    // console.log("values: ", values);
 
-        let price = products[i].price;
-        
-        let productId = products[i].id;
-        // console.log("json:",jsonobject);
-        // console.log("values: ", values);
-
-        // if (category === "men's clothing") {
-        productsEl.innerHTML += `
+    // if (category === "men's clothing") {
+    productsEl.innerHTML += `
         <div class="col-6 col-md-6 col-lg-4 mb-3">
         <div class="card h-100">
         <img src="${productImage}" class="productItem card-img"  alt="${productTitle}" style="height: 300px; width: 100%; object-fit: contain;>
@@ -103,7 +100,9 @@ function renderJson(json1) {
          aria-controls="offcanvasWithBothOptions" onclick="addToCart(${productId}); showBasket()">
         <div class="card-body m-0 mb-0">
         <div class="card-title">
-        <p class="card-text text-truncate" style="max-width: 150px;"><b id="title${i+1}">${productTitle}</b></p>
+        <p class="card-text text-truncate" style="max-width: 150px;"><b id="title${
+          i + 1
+        }">${productTitle}</b></p>
         <div class="d-inline-flex ustify-content-between">
         <p><b>$${price}</b></p>
         
@@ -115,26 +114,25 @@ function renderJson(json1) {
         </div>
         </div>
         </div>
-            `;
-        // }
-        //<div class="col-6 col-md-6 col-lg-4 mb-3">
-        // <div class="card h-100">
-        //     <a role="button" data-bs-toggle="modal" data-bs-target="#picturemodal${i}">
-        //         <img class="m-3" src="${productImage}" alt="${productTitle}" style="height: 300px; width: 100%; object-fit: contain;></a>
-            
-        //     <div class="card-body m-3 mb-0">
-        //         <div class="card-title">
-        //             <p class="card-text text-truncate" style="max-width: 150px;"><b>${productTitle}</b></p>
-        //             <p><b>${price} kr.</b></p>
-        //         </div>
-        // </div>
-        //    </div > 
-        //<div class="card-img-overlay">
-        //     <img src="add-to-cart.png" class="float-end rounded-circle rounded-circle2 bg-dark-subtle" id='a1' role="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions"
-        //         aria-controls="offcanvasWithBothOptions" onclick="addToCart(${productId}); showBasket()">
-        // </div>
-        modalEl.innerHTML +=
             `
+    // }
+    //<div class="col-6 col-md-6 col-lg-4 mb-3">
+    // <div class="card h-100">
+    //     <a role="button" data-bs-toggle="modal" data-bs-target="#picturemodal${i}">
+    //         <img class="m-3" src="${productImage}" alt="${productTitle}" style="height: 300px; width: 100%; object-fit: contain;></a>
+
+    //     <div class="card-body m-3 mb-0">
+    //         <div class="card-title">
+    //             <p class="card-text text-truncate" style="max-width: 150px;"><b>${productTitle}</b></p>
+    //             <p><b>${price} kr.</b></p>
+    //         </div>
+    // </div>
+    //    </div >
+    //<div class="card-img-overlay">
+    //     <img src="add-to-cart.png" class="float-end rounded-circle rounded-circle2 bg-dark-subtle" id='a1' role="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions"
+    //         aria-controls="offcanvasWithBothOptions" onclick="addToCart(${productId}); showBasket()">
+    // </div>
+    modalEl.innerHTML += `
         <div class="modal fade" id="picturemodal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -155,86 +153,80 @@ function renderJson(json1) {
                 </div>
             </div>
         </div>
-        `}
+        `
+  }
 }
-;
-
-
 //cart array
-let cart = JSON.parse(localStorage.getItem("CART")) || [];
-console.log("cart", cart);
-console.log("cart.length", cart.length);
+let cart = JSON.parse(localStorage.getItem('CART')) || []
+console.log('cart', cart)
+console.log('cart.length', cart.length)
 
 if (cart.length < 1) {
-    basketEl.className = "invisible";
-
+  basketEl.className = 'invisible'
 } else {
-    basketEl.className = "";
-
+  basketEl.className = ''
 }
-updateCart();
-showCartIcon();
+updateCart()
+showCartIcon()
 //functions
 
 function addToCart(id) {
+  let productsLocal = localStorage.getItem('products')
+  let products = JSON.parse(productsLocal)
+  console.log('products: ', products)
+  console.log(id)
+  basketEl.scrollIntoView()
 
-    let productsLocal = localStorage.getItem("products");
-    let products = JSON.parse(productsLocal);
-    console.log("products: ", products);
-    console.log(id);
-    basketEl.scrollIntoView();
-
-    //check if product already exists in cart
-    if (cart.some((item) => item.id === id)) {
-        alert("Product already in cart")
-    }
-    else {
-        const item = products.find((product) => product.id === id)
-        // console.log(item);
-        cart.push({
-            ...item,
-            numberOfUnits: 1,
-        });
-        console.log(cart);
-    }
-    updateCart();
+  //check if product already exists in cart
+  if (cart.some((item) => item.id === id)) {
+    alert('Product already in cart')
+  } else {
+    const item = products.find((product) => product.id === id)
+    // console.log(item);
+    cart.push({
+      ...item,
+      numberOfUnits: 1,
+    })
+    console.log(cart)
+  }
+  updateCart()
 }
 //update cart
 function updateCart() {
-    renderCartItems();
-    renderSubTotal();
-    showCartIcon();
+  renderCartItems()
+  renderSubTotal()
+  showCartIcon()
 
-
-    //sace cart to local storage
-    localStorage.setItem("CART", JSON.stringify(cart));
+  //sace cart to local storage
+  localStorage.setItem('CART', JSON.stringify(cart))
 }
 
 // calculate and render subtotal
 function renderSubTotal() {
-    let totalPrice = 0, totalItems = 0;
-    cart.forEach(item => {
-        totalPrice += item.price * item.numberOfUnits * localStorage.getItem("USD");
-        totalItems += item.numberOfUnits;
-    });
-    subtotalEl.innerHTML =
-        `
+  let totalPrice = 0,
+    totalItems = 0
+  cart.forEach((item) => {
+    totalPrice += item.price * item.numberOfUnits * localStorage.getItem('USD')
+    totalItems += item.numberOfUnits
+  })
+  subtotalEl.innerHTML = `
     
-        <div  class="d-flex align-self-flex-end"><p class="m-0"><b><Em>Subtotal(${totalItems} items): ${totalPrice.toFixed(0)} kr.</em></b></p>
+        <div  class="d-flex align-self-flex-end"><p class="m-0"><b><Em>Subtotal(${totalItems} items): ${totalPrice.toFixed(
+    0
+  )} kr.</em></b></p>
         </div>
         <a href="/checkout.html"><button id="checkoutbutton" class="btn btn-secondary p-1 h-100 w-100 rounded-0"><b><em>Proceed to checkout</em></b></button></a>
         `
-    totalItemsInCartEl.innerHTML = totalItems;
-};
+  totalItemsInCartEl.innerHTML = totalItems
+}
 
 // render cart items
 function renderCartItems() {
-    basketlistEl.innerHTML = "";
-    cart.forEach((item) => {
-        let price = item.price * localStorage.getItem("USD");
-        price = price.toFixed(0);
-        basketlistEl.innerHTML +=
-            `
+  basketlistEl.innerHTML = ''
+  cart.forEach((item) => {
+    let price = item.price * localStorage.getItem('USD')
+    price = price.toFixed(0)
+    basketlistEl.innerHTML += `
            <td><img src="${item.image}" alt="${item.title}" onclick="removeItemFromCart(${item.id})" style="max-width: 50px"></td>
             <td>${item.title}</td>
             <td><b>${price} kr.</b></td>
@@ -242,65 +234,60 @@ function renderCartItems() {
            <td><b>${item.numberOfUnits}</b></td><td><a id='a1' role="button" 
            class='btn btn-light rounded-circle' onclick="changeNumberOfUnits('plus', ${item.id})">+</a></td>
         `
-    });
+  })
 }
 //change number of units for an item
 
 function changeNumberOfUnits(action, id) {
-    cart = cart.map((item) => {
-        console.log(item.numberOfUnits);
-        let numberOfUnits = item.numberOfUnits;
-        console.log(numberOfUnits);
-        console.log("item.id: ", item.id);
-        console.log("id: ", id);
-        if (item.id === id) {
-            console.log("action: ", action);
-            if (action === "minus" && numberOfUnits > 1) {
-                numberOfUnits--;
-                console.log("numberOfUnits: ", numberOfUnits);
-            } else if (action === "plus") {
-                numberOfUnits++;
-            }
-        }
-        console.log("item", item);
-        return {
-            ...item,
-            numberOfUnits,
-        }
-    });
+  cart = cart.map((item) => {
+    console.log(item.numberOfUnits)
+    let numberOfUnits = item.numberOfUnits
+    console.log(numberOfUnits)
+    console.log('item.id: ', item.id)
+    console.log('id: ', id)
+    if (item.id === id) {
+      console.log('action: ', action)
+      if (action === 'minus' && numberOfUnits > 1) {
+        numberOfUnits--
+        console.log('numberOfUnits: ', numberOfUnits)
+      } else if (action === 'plus') {
+        numberOfUnits++
+      }
+    }
+    console.log('item', item)
+    return {
+      ...item,
+      numberOfUnits,
+    }
+  })
 
-    updateCart();
+  updateCart()
 }
 // remove item from cart
 function removeItemFromCart(id) {
-    console.log("removeItemFromCart");
-    cart = cart.filter((item) => item.id !== id);
-    if (cart.length === 0) {
-        basketEl.className = "invisible"
-    }
+  console.log('removeItemFromCart')
+  cart = cart.filter((item) => item.id !== id)
+  if (cart.length === 0) {
+    basketEl.className = 'invisible'
+  }
 
-    updateCart();
-    showCartIcon();
-    emptyCart();
-
+  updateCart()
+  showCartIcon()
+  emptyCart()
 }
 function showBasket() {
-    console.log("showBasket");
-    basketEl.className = "";
-
+  console.log('showBasket')
+  basketEl.className = ''
 }
 function showCartIcon() {
-
-    if (cart.length < 1) {
-        carticonEl.className = "visually-hidden";
-
-    } else {
-        carticonEl.className = "visible d-flex position-relative";
-
-    }
+  if (cart.length < 1) {
+    carticonEl.className = 'visually-hidden'
+  } else {
+    carticonEl.className = 'visible d-flex position-relative'
+  }
 }
-function emptyCart(){
-    if(cart.length ===0){
-        modalBasketEl.innerHTML = `<p>Your basket is empty</p>`
-    }
+function emptyCart() {
+  if (cart.length === 0) {
+    modalBasketEl.innerHTML = `<p>Your basket is empty</p>`
+  }
 }
